48a723fc991e28a8cc61dbf0761ec1df99397eea
Oleksandr Yunoshev <yunoshev.alex@gmail.com>
2021-10-19 13:44:26 +0300

    update models

--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -6,14 +6,11 @@ namespace Domain.Models
     public class Post : IEntity<int>
     {
         public int Id { get; set; }
-        public User User { get; set; }
-
-        public List<Hobby> HobbyList { get; set; }
         public string Caption { get; set; }
         public string Description { get; set; }
         public string PhotoPath { get; set; }
+        public List<Hobby> HobbyList { get; set; }
         public List<Like> LikesList { get; set; }
         public List<Comment> CommentsList { get; set; }
-
     }
 }
--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -6,13 +6,14 @@ namespace Domain.Models
     public class User : IEntity<int>
     {
         public int Id { get; set; }
+        public int Age { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string PhotoPath { get; set; }
         public Location Location { get; set; }
-        public List<User> FriendsList { get; set; }
         public List<Post> PostsList { get; set; }
+        //public List<User> FriendsList { get; set; }
     }
 }
