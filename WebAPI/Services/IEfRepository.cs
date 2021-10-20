8187fce60a760abf2913c6e84a6fdd189fea643f
Oleksandr Yunoshev <yunoshev.alex@gmail.com>
2021-10-20 17:25:27 +0300

    try to add jwt aut

---
+++ WebAPI/Services/IEfRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public interface IEfRepository<T> where T : BaseEntity
+    {
+        List<T> GetAll();
+        T GetById(long id);
+        Task<long> Add(T entity);
+    }
+}
