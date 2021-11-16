f0686528e7183f86f8813e2eebc880572b7841e1
Oleksandr Yunoshev <yunoshev.alex@gmail.com>
2021-11-16 14:43:25 +0200

    add ExplorePost, ExploreLike and ExploreComment models

---
+++ Domain/Models/ExploreComment.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interfaces;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public class ExploreComment : IEntity<int>
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        public int PostId { get; set; }
+
+        //[ForeignKey("UserId")]
+        public User User { get; set; }
+
+        //[ForeignKey("PostId")]
+        public ExplorePost Post { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ HOBBYNetMVC/Controllers/ExplorePostsController.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HOBBYNetMVC.Controllers
+{
+    public class ExplorePostsController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ExplorePostsService _explorePostsService;
+
+        public ExplorePostsController(UserManager<User> userManager, SignInManager<User> signInManager, ExplorePostsService explorePostsService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _explorePostsService = explorePostsService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //var loginUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            //var hobbies = _hobbyService.GetUserHobbiesList(loginUserId);
+            //return View(hobbies);
+        }
+    }
+}
