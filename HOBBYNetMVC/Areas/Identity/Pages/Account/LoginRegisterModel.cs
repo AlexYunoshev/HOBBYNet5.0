2e202947736174212470e0056778b32ed840e424
Oleksandr Yunoshev <yunoshev.alex@gmail.com>
2021-11-21 16:51:37 +0200

    try to change my account controller to identity/account

---
+++ HOBBYNetMVC/Areas/Identity/Pages/Account/LoginRegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOBBYNetMVC.Areas.Identity.Pages.Account
+{
+    public class LoginRegisterModel
+    {
+        public LoginModel loginModel { get; set; }
+        public RegisterModel registerModel { get; set; }
+    }
+}
