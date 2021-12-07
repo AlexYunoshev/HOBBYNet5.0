85aaf7e478574f4e733ca66ff46bca5700ac61ad
Oleksandr Yunoshev <yunoshev.alex@gmail.com>
2021-12-07 15:05:56 +0200

    add basic advert body

---
+++ BusinessLogic/Services/AdvertService.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class AdvertService
+    {
+        private readonly HobbyNetContext _context;
+
+        public AdvertService(HobbyNetContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ HOBBYNetMVC/Controllers/AdvertController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLogic.Services;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOBBYNetMVC.Controllers
+{
+    public class AdvertController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserService _userService;
+        private readonly ExplorePostsService _explorePostsService;
+        private readonly ILogger<UserController> _logger;
+        private readonly HobbyService _hobbyService;
+
+
+        public AdvertController(
+            UserManager<User> userManager, SignInManager<User> signInManager,
+            UserService userService, ExplorePostsService explorePostsService,
+            HobbyService hobbyService,
+            ILogger<UserController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _userService = userService;
+            _explorePostsService = explorePostsService;
+            _hobbyService = hobbyService;
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
